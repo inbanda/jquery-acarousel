c41eeb379a35562acde10ac87f96f957bb52e205
aldocoria <aldocoria@gmail.com>
2013-04-25 21:15:11 -0300

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    $('#acarousel-wrapper').aCarousel({
-		"auto" : true,
+	$('#acarousel-wrapper').aCarousel({
+        "auto" : true,
 		"delay" : 3000
-    });
-};
+	});
+});
