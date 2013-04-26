7b22da1ba9895e4520203b771cb41b48db82fced
aldocoria <aldocoria@gmail.com>
2013-04-25 21:08:23 -0300

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	$('#acarousel-wrapper').aCarousel({
-    auto: true,
-		delay: 4000
-	});
-});
+    $('#acarousel-wrapper').aCarousel({
+		"auto" : true,
+		"delay" : 3000
+    });
+};
